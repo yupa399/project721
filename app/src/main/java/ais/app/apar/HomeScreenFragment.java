874db01c5506917daad0aa2f1481f5ced4301d3c
pyunqi <pyunqi@gmail.com>
2019-09-29 15:30:33 +1300

    project init with basic app

---
+++ app/src/main/java/ais/app/apar/HomeScreenFragment.java
@@ -0,0 +1,26 @@
+package ais.app.apar;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeScreenFragment extends Fragment {
+
+
+    public HomeScreenFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+    }
+
+}
---
+++ app/src/main/java/ais/app/apar/SplashActivity.java
@@ -0,0 +1,34 @@
+package ais.app.apar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+}
